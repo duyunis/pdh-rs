a0fed937a8b61d25475c7c1841083de46381a7be
Yunis <yunis@88.com>
2023-04-26 15:47:53 +0800

    Custom print flush macro

--- src/common/mod.rs
+++ src/common/mod.rs
@@ -1,2 +1,25 @@
+use thiserror::Error;
+
 pub mod consts;
 pub mod version;
+
+pub type Result<T, E = Error> = std::result::Result<T, E>;
+
+
+#[derive(Debug, Error)]
+pub enum Error {
+    #[error("{0}: No such file or directory.")]
+    FileNotFound(String),
+    #[error("{0}")]
+    Other(String),
+}
+
+
+#[macro_export]
+macro_rules! print_flush {
+    ($($arg:tt)*) => {{
+        use std::io::{Write, stdout};
+        print!($($arg)*);
+        stdout().flush().unwrap();
+    }};
+}
